13f4bad004fa274daee84713f1516a10453808c4
laozishibeigua <2386120070@qq.com>
2026-03-15 16:03:44 +0800

    feat: implemented basic query functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import httpx
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -10,15 +10,87 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("tetr")
 
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+    def get_gametime(data):
+        if "gametime" not in data:
+            return "没有游戏时间数据捏..."
+        
+        gametime_seconds = data["gametime"]
+        if gametime_seconds == -1:
+            return "人家不想告诉你啦！"
+        
+        hours = gametime_seconds // 3600
+        return f"{hours} 小时"
+
+
+    def get_gamesplayed(data):
+        if "gamesplayed" not in data:
+            return "没有游戏局数数据捏..."
+        
+        gamesplayed = data["gamesplayed"]
+        
+        return str(gamesplayed)
+
+
+    def get_gameswon(data):
+        if "gameswon" not in data:
+            return "没有游戏胜利数据捏..."
+        
+        gameswon = data["gameswon"]
+        
+        return str(gameswon)
+    
+    def get_league_rating(user_name):
+        lg_r = httpx.get(f"https://ch.tetr.io/api/users/{user_name}/summaries/league")
+        if lg_r.status_code != 200:
+            return "获取rating失败了捏..."
+        
+        lg_result_data = lg_r.json()
+
+        if lg_result_data["success"] == False:
+            return "获取rating失败了捏..."
+
+        if "data" not in lg_result_data:
+            return "获取rating失败了捏..."
+        
+        lg_data = lg_result_data["data"]
+        if "glicko" not in lg_data:
+            return "获取rating失败了捏..."
+
+        league_rating = lg_data["glicko"]
+        if league_rating == -1:
+            return "打的太少，木有rating捏！"
+        return str(league_rating)
+
+    async def get_tetrio_user_info(self, event: AstrMessageEvent, user_name:str):
+        """获取 TETR.IO 用户信息"""
+        r = httpx.get(f"https://ch.tetr.io/api/users/{user_name}")
+        
+        if r.status_code != 200:
+            await event.send(event.plain_result(f"获取用户信息失败，状态码: {r.status_code}"))
+            return
+        
+        result_data = r.json()
+
+        if result_data["success"] == False:
+            await event.send(event.plain_result(f"获取用户信息失败，错误信息: {data['error']}"))
+            return
+        if "data" not in result_data:
+            await event.send(event.plain_result("获取用户信息失败，不知道为啥木有数据捏"))
+            return
+        
+        data = result_data["data"] 
+
+        gametime_info = self.get_gametime(data)
+        gamesplayed_info = self.get_gamesplayed(data)
+        gameswon_info = self.get_gameswon(data)
+        league_rating_info = self.get_league_rating(user_name)
+        
+        final_message = f"{user_name} 的 tetr.io 信息："
+        final_message += f"\n游戏时间: {gametime_info}"
+        final_message += f"\n游戏局数: {gamesplayed_info}"
+        final_message += f"\n胜利局数: {gameswon_info}"
+        final_message += f"\n联赛rating: {league_rating_info}"
+
+        await event.send(event.plain_result(final_message))
